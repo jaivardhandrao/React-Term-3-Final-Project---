84ff6a3b568630636fe3368c49c67b736f48bf76
jaivardhandrao <jaivardhandrao@gmail.com>
2025-04-06 17:15:34 +0530

    test commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function App() {
   return (
     <>
-    <div>App</div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <div>App</div>
+      <h1 class="text-3xl font-bold underline">
+        Hello jaivardhan how are you!
+      </h1>
     </>
   )
 }
