5c7157fd4848421a4ffc6c9db3b9fcb716027480
jaivardhandrao <jaivardhandrao@gmail.com>
2025-04-19 01:33:19 +0530

    deploy commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  base: "React-Term-3-Final-Project---",
+  plugins: [tailwindcss(), react()],
 })
+
